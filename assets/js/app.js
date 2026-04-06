66e6ea43abb21d6060b1aa2838436fdcd297cabc
RamOkami <1franchute2004@gmail.com>
2026-04-06 19:19:58 -0400

    se agrego el login pop con html y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,39 @@
-let logins = document.getElementById('logins');
-let perfil = document.getElementById('perfil');
+const modal = document.getElementById('auth_modal');
+const loginBtn = document.getElementById('button_log');
+const registerBtn = document.getElementById('button_reg');
+const closeBtn = document.getElementById('cerrar_pop');
+const modalTitle = document.getElementById('titulo_pop');
+const submitModalBtn = document.getElementById('button_enviar');
 
-let buttons_log = document.querySelectorAll('button.button_log');
+const profileImg = document.getElementById('perfil');
 
-buttons_log.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        logins.classList.add('hidden');
-        perfil.classList.remove('hidden');
-    })
-});
+//funcion para abrir el pop de login
+function open_pop(type){
+    modal.classList.remove('hidden');
+
+    if(type === 'login'){
+        modalTitle.textContent = 'Iniciar Sesion';
+        submitModalBtn.textContent = 'ENTRAR';
+    } else if (type === 'register') {
+        modalTitle.textContent = 'Crear Cuenta';
+        submitModalBtn.textContent = 'REGISTRARSE';
+    }
+} 
+
+//funcion paracerrar el pop
+function close_pop() {
+    modal.classList.add('hidden');
+}
+
+function submit_pop() {
+    loginBtn.classList.add('hidden');
+    registerBtn.classList.add('hidden');
+    profileImg.classList.remove('hidden');
+    close_pop();
+}
+
+loginBtn.addEventListener('click', () => {open_pop('login')});
+registerBtn.addEventListener('click', () => {open_pop('register')});
+closeBtn.addEventListener('click', () => {close_pop()});
+
+submitModalBtn.addEventListener('click', () => {submit_pop()});
